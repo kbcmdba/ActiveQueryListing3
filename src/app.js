fd61bae9b19bfeec7925f4e0e12827b4074fd3a7
Kevin Benton <kbcmdba@gmail.com>
2021-06-15 21:04:20 -0400

    Testing

--- src/app.js
+++ src/app.js
@@ -2,71 +2,53 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
-
 const app = express()
 const port = process.env.PORT || 8080
+
+const expressOptions = {
+  dotfiles: 'ignore',
+  extensions: ['htm', 'html'],
+  index: false
+}
+app.use(express.static('public', expressOptions))
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
+const appTitle = 'Activ Query Listing 3'
+const appAuthor = 'Kevin Benton'
+
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+// hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Kevin Benton'
-    })
+    res.send('Hello world!\n')
+//    res.render('index', {
+//        title: appTitle,
+//        name: appAuthor
+//    })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About Me',
-        name: 'Kevin Benton'
+        title: 'About AQL3',
+        name: appAuthor
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help Page',
-        name: 'Kevin Benton',
+        name: appAuthor,
         helpMsg: 'This is some help.'
     })
-})
-
-app.get('/weather', (req, res) => {
-    const address = req.query.address
-    if (!address) {
-        return res.send({
-            error: 'You must provide a valid address to search.'
-        })
-    }
-
-    geocode(address, (error, {latitude, longitude, location} = {}) => {
-        if (error) {
-            return res.send({ error })
-        }
-        forecast(latitude, longitude, (error, { forecast }) => {
-            if (error) {
-                return res.send({ error })
-            }
-            return res.send({
-                location,
-                forecast: forecast,
-                address
-            })
-        }) 
-    })
-
 })
 
 app.get('/products', (req, res) => {
