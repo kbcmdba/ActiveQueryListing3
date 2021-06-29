76f7d17f691419220207c6fe197673a2f37de18f
Kevin Benton <kbcmdba@gmail.com>
2021-06-29 14:17:45 -0400

    Moving forward with nunjucks

--- src/app.js
+++ src/app.js
@@ -1,57 +1,76 @@
 const path = require('path')
 const express = require('express')
-const hbs = require('hbs')
-
+const nunjucks = require('nunjucks')
 const app = express()
 const port = process.env.PORT || 8080
-
-const expressOptions = {
-  dotfiles: 'ignore',
-  extensions: ['htm', 'html'],
-  index: false
-}
-app.use(express.static('public', expressOptions))
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
+app.use( express.static( publicDirectoryPath, {
+  dotfiles: 'ignore',
+  extensions: ['htm', 'html'],
+  index: false
+} ) )
+
 const appTitle = 'Active Query Listing 3 for MySQL'
 const appAuthor = 'Kevin Benton'
 
 const hostList = [ '127.0.0.1', 'rpi6e' ]
 
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
-
-// Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
-
-app.get('', (req, res) => {
-    res.render('index', {
-        title: appTitle,
-        author: appAuthor
-    })
+const env = nunjucks.configure( [ viewsPath ], {
+    autoescape: true, 
+    express: app
 })
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: appTitle,
-        author: appAuthor
-    })
+env.addFilter( 'myFilter', function( obj, arg1, arg2 ) {
+    console.log('myFilter', obj, arg1, arg2 )
+    // Do smth with obj
+    return obj  
+})
+
+env.addGlobal( 'myFunc', function( obj, arg1 ) { 
+    console.log( 'myFunc', obj, arg1 )
+    // Do smth with obj
+    return obj
+})
+
+env.addGlobal( 'getDate', function( obj ) {
+    return new Date().toString()
+} )
+
+app.get( '', function( req, res ) {
+    res.render( 'index.html.j2', {
+        title: appTitle, 
+        author: appAuthor,
+        refreshSeconds: 30
+    } )
+})
+
+app.get( '/', function( req, res ) {
+    res.render( 'index.html.j2', {
+        title: appTitle, 
+        author: appAuthor,
+        refreshSeconds: 60
+    } )
+})
+
+app.get( '/foo', function( req, res ) {
+    res.locals.smthVar = 'This is Sparta!'
+    res.render( 'foo.html.j2', { title: 'Foo page' } )    
 })
 
 app.get('/about', (req, res) => {
-    res.render('about', {
+    res.render('about.html.j2', {
         title: 'About AQL3',
         author: appAuthor
     })
 })
 
 app.get('/help', (req, res) => {
-    res.render('help', {
+    res.render('help.html.j2', {
         title: 'Help Page',
         author: appAuthor,
         helpMsg: 'This is some help.'
